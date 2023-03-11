0c7c7b6dd30a97650c2dcc16adf904fa7b5de73d
ileri-oluwa-kiiye <aboladeilerioluwakiiye@gmail.com>
2023-03-11 19:51:16 +0100

    Okayy. The portfolio is portfolioingg

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
+import { BrowserRouter ,  Route, Routes } from "react-router-dom"
 import './App.css';
 import Home from './scenes/home';
+import About from './scenes/about';
 
 function App() {
   return (
-    <div>
-      <Home />
-    </div>
+    <BrowserRouter>
+      <Routes>
+          <Route exact path="/" element={<Home />} />
+          <Route exact path="/about" element={<About />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
--- src/components/about.jsx
+++ src/components/about.jsx
@@ -6,7 +6,19 @@ const Aboutme = () =>{
     return(
         <div className={styles.wrapdiv}>
             <div className="controlwidth">
-                <img src={aboutme} alt="" />
+
+                <div className={styles.wrap}>
+                    <img src={aboutme} alt="" />
+                    <div className={styles.about}>
+                        <div className={styles.content}>
+                        Hi, I am Sakshi, a barista, chef, a salesgirl, an IT support Specialist and now, finally turning to my dream role UX designer. 
+                        My motto is IDEAS, illustrate designs to empower aspiring solutions. I want to use my skills, designs and creativity to shape 
+                        and enhance the ideas or solutions that aspire to change the society and benefit every person irrespective of their background. 
+                        Graduated from Douglas College in March 2022, I have studied various course works and successfully completed variety of projects 
+                        and worked diverse set of jobs, all of this polishing me towards the bigger goal. Since, my childhood I have this fascination 
+                        </div>
+                    </div>
+                </div>
             </div>
         </div>
     )
