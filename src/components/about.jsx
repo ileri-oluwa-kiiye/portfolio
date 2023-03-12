24e03ab5b2a8df0bcbc50770590fcb3695cb4c3a
ileri-oluwa-kiiye <aboladeilerioluwakiiye@gmail.com>
2023-03-12 01:03:48 +0100

    Used lorem for the meantime

--- src/components/about.jsx
+++ src/components/about.jsx
@@ -11,11 +11,7 @@ const Aboutme = () =>{
                     <img src={aboutme} alt="" />
                     <div className={styles.about}>
                         <div className={styles.content} data-aos="slide-left" data-aos-delay="00" data-aos-duration="1100">
-                        Hi, I am Sakshi, a barista, chef, a salesgirl, an IT support Specialist and now, finally turning to my dream role UX designer. 
-                        My motto is IDEAS, illustrate designs to empower aspiring solutions. I want to use my skills, designs and creativity to shape 
-                        and enhance the ideas or solutions that aspire to change the society and benefit every person irrespective of their background. 
-                        Graduated from Douglas College in March 2022, I have studied various course works and successfully completed variety of projects 
-                        and worked diverse set of jobs, all of this polishing me towards the bigger goal. Since, my childhood I have this fascination 
+                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Nisi ratione vitae tenetur reprehenderit asperiores dignissimos architecto, beatae error distinctio mollitia, deserunt non quod unde incidunt, laudantium veritatis tempora labore ab aspernatur. Maiores, numquam. Excepturi sed deleniti possimus quas quaerat eligendi accusamus officiis autem at itaque mollitia doloribus nostrum architecto doloremque dolore error, molestias atque numquam nemo explicabo fugit tempore! Illo qui mollitia tenetur nisi iste necessitatibus ipsa minus laudantium beatae doloremque deserunt eveniet obcaecati culpa, atque cupiditate saepe illum suscipit?
                         </div>
                     </div>
                 </div>
