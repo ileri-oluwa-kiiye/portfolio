ea86177725820ac68d670bc3e52c35a130db09c9
ileri-oluwa-kiiye <aboladeilerioluwakiiye@gmail.com>
2023-03-13 22:03:57 +0100

    Added the navbar

--- src/components/mainnav.jsx
+++ src/components/mainnav.jsx
@@ -1,18 +1,29 @@
 import logo from "../img/initial.svg"
 import { Link } from 'react-router-dom'
 import styles from "./mainnav.module.css"
+import opennav from "../img/opennav.png"
 
 const Mainnav =() =>{
+
+
     return(
         <div className={styles.nav}>
             <nav>
                 <ul data-aos="slide-right" data-aos-delay="700" data-aos-duration="1100">
-                    <li className={styles.first}><img src={logo} alt="" height={30} /></li>
-                    <Link to="/" style={{color:'inherit', textDecoration: 'none'}}><li>Home</li></Link>
-                    <Link to="/about" style={{color:'inherit', textDecoration: 'none'}}><li>About</li></Link>
-                    <Link to="/projects" style={{color:'inherit', textDecoration: 'none'}}><li>Projects</li></Link>
-                    <Link to="/contact" style={{color:'inherit', textDecoration: 'none'}}><li>Contact</li></Link>
+                    <div className={styles.logo}>
+                        <Link to="/" style={{color:'inherit', textDecoration: 'none'}}><li className={styles.first}><img src={logo} alt="" height={60} /></li></Link>
+                    </div>
+                    <div className={styles.hidemobile}>
+                        <Link to="/" style={{color:'inherit', textDecoration: 'none'}}><li>Home</li></Link>
+                        <Link to="/about" style={{color:'inherit', textDecoration: 'none'}}><li>About</li></Link>
+                        <Link to="/projects" style={{color:'inherit', textDecoration: 'none'}}><li>Projects</li></Link>
+                        <Link to="/contact" style={{color:'inherit', textDecoration: 'none'}}><li>Contact</li></Link>
+                    </div>
                 </ul>
+
+                    <ul className={styles.hidedesktop}>
+                        <li><img src={opennav} alt="" height={23} width={30} /></li>
+                    </ul>
             </nav>
         </div>
     )
