e9afd019a7292789e57879a6b3e238176c697b50
ileri-oluwa-kiiye <aboladeilerioluwakiiye@gmail.com>
2023-03-13 09:06:59 +0100

    Projects

--- src/components/about.jsx
+++ src/components/about.jsx
@@ -11,7 +11,7 @@ const Aboutme = () =>{
                     <img src={aboutme} alt="" />
                     <div className={styles.about}>
                         <div className={styles.content} data-aos="slide-left" data-aos-delay="00" data-aos-duration="1100">
-                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Nisi ratione vitae tenetur reprehenderit asperiores dignissimos architecto, beatae error distinctio mollitia, deserunt non quod unde incidunt, laudantium veritatis tempora labore ab aspernatur. Maiores, numquam. Excepturi sed deleniti possimus quas quaerat eligendi accusamus officiis autem at itaque mollitia doloribus nostrum architecto doloremque dolore error, molestias atque numquam nemo explicabo fugit tempore! Illo qui mollitia tenetur nisi iste necessitatibus ipsa minus laudantium beatae doloremque deserunt eveniet obcaecati culpa, atque cupiditate saepe illum suscipit?
+                        As a highly motivated frontend developer with an insatiable desire for knowledge, I am dedicated to expanding my skills and knowledge in this field. I possess a very strong foundation in HTML, CSS and JavaScript, and I’m well versed in popular frontend frameworks and libraries, like React and bootstrap. With an unwavering commitment to continuous learning and professional growth, I am eager to contribute my skills and enthusiasm to any project I am involved in.
                         </div>
                     </div>
                 </div>
--- src/components/projectpage.jsx
+++ src/components/projectpage.jsx
@@ -1,9 +1,56 @@
-// import styles from "./project.module.css"
+import  "./project.css"
+import crappo from "../img/projects/crappo.png"
+import cruvbet from "../img/projects/cruvbet.png"
+import fintraka from "../img/projects/fintraka.png"
+// import ecommerce from "../img/projects/ecommerce.png"
+// import questio from "../img/projects/questio.png"
+// import todo from "../img/projects/todo.png"
 
 const Projectpage = () =>{
     return(
         <div>
-            Hey
+            <section className="portfolio">
+                    <h2 data-aos="zoom-in" data-aos-duration="2000">
+                        SOME OF THE PROJECTS I'VE WORKED ON
+                    </h2>
+                    <div>
+                        <div className="flexdesktop">
+                            <div>
+                                <h3 data-aos="zoom-in">Crappo App</h3>
+                                <p data-aos="zoom-in">
+                                    An imaginary cryptocurrency website
+                                </p>
+                            </div>
+                            <div>
+                                <img src={crappo} alt="Invest Smart" data-aos="zoom-in" data-aos-duration="2000"/>
+                            </div>
+                        </div>
+                        <div className="flexdesktop rowreverse">
+                            <div>
+                                <h3 data-aos="zoom-in">Fin-traka Web App</h3>
+                                <p data-aos="zoom-in">
+                                    Loren ipsum dolor sit amet consectetur adipisicing elit.
+                                    Libero deserunt<br/> commodi quos adipisci! Corporis, vero?
+                                </p>
+                            </div>
+                            <div>
+                                <img src={fintraka} alt="Detailed Statistics" data-aos="zoom-in" data-aos-duration="2000"/>
+                            </div>
+                        </div>
+                        <div className="flexdesktop">
+                            <div>
+                                <h3 data-aos="zoom-in">Cruvbet</h3>
+                                <p data-aos="zoom-in">
+                                    Loren ipsum<br/> dolor sit amet consectetur adipisicing elit.
+                                    Sit enim odit distinctio dolore id incidunt?
+                                </p>
+                            </div>
+                            <div >
+                                <img src={cruvbet} alt="Detailed Statistics" data-aos="zoom-in" data-aos-duration="2000"/>
+                            </div>
+                        </div>
+                    </div>
+                </section>
         </div>
     )
 }
